191f70ef6a659227b6a30d65f21aaba488cb6a9f
ernestpazera <ernestpazera@gmail.com>
2020-10-30 03:57:56 -0500

    added publishing script for itch

--- log.md
+++ log.md
@@ -1,4 +1,7 @@
-Day #1: Test Driven Development in F# of a Text-Based City Management Game
+Day #2: Test Driven Development in F# of a Text-Based City Management Game
+# Day 2 - 20201030
+
+* 
 
 # Day 1 - 20201029
 
--- publish.sh
+++ publish.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-dotnet publish -c Release --runtime win-x64 --framework netcoreapp3.1 -o ./deploy/win ./Splorr.Kibbutz/Splorr.Kibbutz.fsproj
-dotnet publish -c Release --runtime osx-x64 --framework netcoreapp3.1 --self-contained true -o ./deploy/mac ./Splorr.Kibbutz/Splorr.Kibbutz.fsproj
-dotnet publish -c Release --runtime linux-x64 --framework netcoreapp3.1 --self-contained true -o ./deploy/linux ./Splorr.Kibbutz/Splorr.Kibbutz.fsproj
-dotnet publish -c Release --runtime linux-arm64 --framework netcoreapp3.1 --self-contained true -o ./deploy/pi ./Splorr.Kibbutz/Splorr.Kibbutz.fsproj
+rm -rf ./deploy/win/*
+rm -rf ./deploy/mac/*
+rm -rf ./deploy/linux/*
+rm -rf ./deploy/pi/*
+rm -rf ./deploy/*.zip
+
+dotnet publish -c Release --runtime win-x64 -o ./deploy/win ./Splorr.Kibbutz/Splorr.Kibbutz.fsproj -p:PublishReadyToRun=true
+dotnet publish -c Release --runtime osx-x64 --framework netcoreapp3.1 --self-contained true  -o ./deploy/mac ./Splorr.Kibbutz/Splorr.Kibbutz.fsproj
+dotnet publish -c Release --runtime linux-x64 --framework netcoreapp3.1 --self-contained true  -o ./deploy/linux ./Splorr.Kibbutz/Splorr.Kibbutz.fsproj
+dotnet publish -c Release --runtime linux-arm64 --framework netcoreapp3.1 --self-contained true  -o ./deploy/pi ./Splorr.Kibbutz/Splorr.Kibbutz.fsproj
 
 rm ./deploy/win/*.pdb
 rm ./deploy/mac/*.pdb
 rm ./deploy/linux/*.pdb
 rm ./deploy/pi/*.pdb
-rm ./deploy/*.zip
 "C:\Program Files\7-Zip\7z" a -tzip ./deploy/Kibbutz-win.zip ./deploy/win/*
 "C:\Program Files\7-Zip\7z" a -tzip ./deploy/Kibbutz-mac.zip ./deploy/mac/*
 "C:\Program Files\7-Zip\7z" a -tzip ./deploy/Kibbutz-linux.zip ./deploy/linux/*
