b8db8533667f0dd61e726640fe4af95c8a20f99f
ernestpazera <ernestpazera@gmail.com>
2020-10-30 07:18:36 -0500

    fixed the name of a test

--- Splorr.Kibbutz.Persistence.Tests/Settlement/PutForSessionTests.fs
+++ Splorr.Kibbutz.Persistence.Tests/Settlement/PutForSessionTests.fs
@@ -5,7 +5,7 @@ open Splorr.Kibbutz.Persistence
 open Splorr.Kibbutz.Business
 
 [<Test>]
-let ``PutSettlementForSession..`` () =
+let ``PutSettlementForSession.It replaces the value for the settlement for a given session.`` () =
     SettlementImplementation.PutSettlementForSession(Dummies.ValidSessionIdentfier, Some { iExistOnlyToHaveAFieldInTheRecord=0})
     SettlementImplementation.PutSettlementForSession(Dummies.ValidSessionIdentfier, None)
     let  actual = SettlementImplementation.GetSettlementForSession Dummies.ValidSessionIdentfier
