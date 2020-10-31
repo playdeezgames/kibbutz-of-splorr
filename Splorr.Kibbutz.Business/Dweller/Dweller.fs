a471b0b83fa4ca04c81febc450525afd9c9af4da
ernestpazera <ernestpazera@gmail.com>
2020-10-31 07:54:40 -0500

    first shot at making names for dwellers works but is ... stupid

--- Splorr.Kibbutz.Business/Dweller/Dweller.fs
+++ Splorr.Kibbutz.Business/Dweller/Dweller.fs
@@ -5,6 +5,11 @@ open System
 open Splorr.Kibbutz.Model
 
 module internal Dweller =
+    let private DescribeName
+            (identifier : DwellerIdentifier)
+            : string =
+        identifier.ToString().Replace('0','o').Replace('1','i').Replace('2','z').Replace('3','m').Replace('4','h').Replace('5','s').Replace('6','k').Replace('7','l').Replace('8','j').Replace('9','g')
+
     let private DescribeSexGenes
             (sexGenes : SexGenes option)
             : string =
@@ -28,7 +33,7 @@ module internal Dweller =
             session
             [
                 Line ""
-                Line (sprintf "Dweller: %s" (identifier.ToString()))
+                Line (sprintf "Dweller: %s" (identifier |> DescribeName))
                 Line (sprintf "Sex: %s" (dweller.sexGenes |> DescribeSexGenes))
             ]
 
