ecb1015b30b47965b8f0424a9cd84cc908d58161
ernestpazera <ernestpazera@gmail.com>
2020-11-05 08:37:57 -0600

    added some help to the help commands

--- Splorr.Kibbutz.Presentation/CommandHandlers/HelpCommandHandler.fs
+++ Splorr.Kibbutz.Presentation/CommandHandlers/HelpCommandHandler.fs
@@ -13,6 +13,9 @@ module internal HelpCommandHandler =
         [
             Hued (Yellow, Line ("abandon - abandons a settlement"))
             Hued (Yellow, Line ("advance - advances a settlement by one turn"))
+            Hued (Yellow, Line ("assign [name] to [assignment] - assigns the dweller with the given name to the given assignment"))
+            Hued (Yellow, Line ("dweller [name] - describes the state of the dweller with the given name"))
+            Hued (Yellow, Line ("dwellers - lists the dwellers in the settlement"))
             Hued (Yellow, Line ("help - shows help"))
             Hued (Yellow, Line ("start - starts a settlement"))
             Hued (Yellow, Line ("quit - quits the game"))
