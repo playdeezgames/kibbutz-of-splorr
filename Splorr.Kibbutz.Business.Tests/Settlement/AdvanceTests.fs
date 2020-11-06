b8af04c20a16e055c19fb45ce0f31b75e72cbcc6
ernestpazera <ernestpazera@gmail.com>
2020-11-06 05:36:48 -0600

    it logs and reports out what each dweller does when the turn advances

--- Splorr.Kibbutz.Business.Tests/Settlement/AdvanceTests.fs
+++ Splorr.Kibbutz.Business.Tests/Settlement/AdvanceTests.fs
@@ -17,12 +17,24 @@ let ``Advance.It returns a message when the session has no settlement.`` () =
 let ``Advance.It advances an existing settlement by one turn.`` () =
     let calledGetSettlement = ref false
     let calledPutSettlement = ref false
+    let calledGetDwellerList = ref false
+    let callsForGetDweller = ref 0UL
+    let callsForLogForDweller = ref 0UL
     let context = Contexts.TestContext()
+    (context :> DwellerRepository.LogForDwellerContext).dwellerLogSink :=
+        Spies.SinkCounter(callsForLogForDweller)
+    (context :> DwellerRepository.GetListForSessionContext).sessionDwellerSource := 
+        Spies.Source(calledGetDwellerList, Dummies.ValidDwellerIdentifiers)
+    (context :> DwellerRepository.GetContext).dwellerSingleSource :=
+        Spies.SourceTable(callsForGetDweller, Dummies.ValidDwellerTable)
     (context :> SettlementRepository.GetSettlementForSessionContext).settlementSource := 
         Spies.Source(calledGetSettlement, Some Dummies.ValidSettlement)
     (context :> SettlementRepository.PutSettlementForSessionContext).settlementSink := 
         Spies.Expect(calledPutSettlement, (Dummies.ValidSessionIdentifier, Some { Dummies.ValidSettlement with turnCounter=Dummies.ValidSettlement.turnCounter + 1UL }))
     let actual = Settlement.Advance context Dummies.ValidSessionIdentifier
-    Assert.AreEqual(1, actual.Length)
+    Assert.AreEqual(4, actual.Length)
     Assert.IsTrue(calledGetSettlement.Value)
     Assert.IsTrue(calledPutSettlement.Value)
+    Assert.IsTrue(calledGetDwellerList.Value)
+    Assert.AreEqual(3UL, callsForGetDweller.Value)
+    Assert.AreEqual(3UL, callsForLogForDweller.Value)
--- Splorr.Kibbutz.Business/Settlement/Advancer.fs
+++ Splorr.Kibbutz.Business/Settlement/Advancer.fs
@@ -5,13 +5,75 @@ open System
 open Splorr.Kibbutz.Model
 
 module internal SettlementAdvancer =
+    let private AdvanceExistingDweller
+            (context : CommonContext)
+            (session : SessionIdentifier)
+            (turn : TurnCounter)
+            (identifier : DwellerIdentifier)
+            (dweller : Dweller)
+            : Message list =
+        match dweller.assignment with
+        | Rest ->
+            DwellerRepository.LogForDweller context (identifier, turn, Line "Rested.")
+            [
+                dweller.name |> sprintf "Dweller %s rests." |> Line
+            ]
+        | Explore ->
+            DwellerRepository.LogForDweller context (identifier, turn, Line "Explored.")
+            [
+                dweller.name |> sprintf "Dweller %s explores." |> Line
+            ]
+
+
+    let private AdvanceDweller
+            (context : CommonContext)
+            (session : SessionIdentifier)
+            (turn : TurnCounter)
+            (identifier : DwellerIdentifier)
+            : Message list =
+        DwellerRepository.Get context identifier
+        |> Option.map (AdvanceExistingDweller context session turn identifier)
+        |> Option.defaultValue []
+
+    let private AdvanceDwellers
+            (context : CommonContext)
+            (session : SessionIdentifier)
+            (turn : TurnCounter)
+            : Message list =
+        DwellerRepository.GetListForSession context session
+        |> List.map (AdvanceDweller context session turn)
+        |> List.reduce (@)
+
+    let private UpdateSettlementTurnCounter
+            (context : CommonContext)
+            (session : SessionIdentifier)
+            (settlement : Settlement)
+            : unit =
+        {settlement with 
+            turnCounter = settlement.turnCounter + 1UL}
+        |> Some
+        |> SettlementRepository.PutSettlementForSession 
+            context 
+            session
+
+    let private AdvanceExistingSettlement
+            (context : CommonContext)
+            (session : SessionIdentifier)
+            (settlement : Settlement)
+            : Message list =
+        let messages = 
+            AdvanceDwellers context session settlement.turnCounter
+        UpdateSettlementTurnCounter context session settlement
+        List.append 
+            messages
+            [Hued (Green, Line "You advance your settlement to the next turn.")]
+
     let internal Advance
             (context : CommonContext)
             (session : SessionIdentifier)
             : Message list =
         match SettlementRepository.GetSettlementForSession context session with
         | Some settlement ->
-            SettlementRepository.PutSettlementForSession context session (Some {settlement with turnCounter = settlement.turnCounter + 1UL})
-            [Hued (Green, Line "You advance your settlement to the next turn.")]
+            AdvanceExistingSettlement context session settlement
         | _ ->
             [Hued (Red, Line "You have no settlement to advance.")]
