c080f35a58885f29e0f4542db21539554c2fa75f
ernestpazera <ernestpazera@gmail.com>
2020-11-09 13:51:26 -0600

    log gets ignored by default but needs not to be ... this time

---
+++ Splorr.Kibbutz.Persistence.Tests/Dweller/Log/PurgeLogsForDwellerTests.fs
@@ -0,0 +1,15 @@
+﻿module Dweller.PurgeLogsForDwellerTests
+
+open NUnit.Framework
+open Splorr.Kibbutz.Persistence
+open Splorr.Kibbutz.Business
+open Splorr.Kibbutz.Model
+
+[<Test>]
+let ``PurgeLogsForDweller.It removes all logs for a given dweller.`` () =
+    DwellerLogStore.LogForDweller
+        (Dummies.ValidDwellerIdentifier, 0UL, Line "I am a message.")
+    DwellerLogStore.PurgeLogsForDweller
+        Dummies.ValidDwellerIdentifier
+    let actual = DwellerLogStore.GetBriefHistory Dummies.ValidDwellerIdentifier
+    Assert.AreEqual([], actual)
